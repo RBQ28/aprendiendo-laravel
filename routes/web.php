26afcffd4fc42617ef8a2f5bc362a44c8b8b4f5e
Richard BQ <richard128bq@gmial.com>
2025-11-10 09:40:43 -0600

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/inicio', function () {
+    return view('inicio');
+});
+
+Route::get('/prueba', function () {
+    return view('prueba');
+});
